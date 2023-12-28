3197af97e390c2e98787dafd8edae0f4da9e199c
thsra001 <tharunsritharun@gmail.com>
2023-12-28 20:04:57 +0100

    mamma mia i fixed it after 5 hours

--- src/player.rs
+++ src/player.rs
@@ -2,6 +2,14 @@ use bevy::prelude::*;
 
 pub struct LocalPlayerManager;
 
+#[derive(Component)]
+struct LocaPlayer;
+#[derive(Component)]
+struct Speed(f32);
+#[derive(Component)]
+struct TypeSymbol;
+
+
 impl Plugin for LocalPlayerManager {
     fn build(&self, app: &mut App) {
         app.add_systems(Startup, make_player);
@@ -10,12 +18,7 @@ impl Plugin for LocalPlayerManager {
 }
 
 fn make_player(mut commands: Commands, asset_server: Res<AssetServer>) {
-    #[derive(Component)]
-    struct LocaPlayer;
-    #[derive(Component)]
-    struct Speed(f32);
-    #[derive(Component)]
-    struct TypeSymbol;
+
 
     // character
     commands
@@ -33,8 +36,8 @@ fn make_player(mut commands: Commands, asset_server: Res<AssetServer>) {
 fn player_movement(
     input: Res<Input<KeyCode>>,
     time: Res<Time>,
-    // mut get_player: Query<&mut Transform, With<LocaPlayer>>,
-    // get_cam: Query<&Transform, (With<Camera3d>, Without<LocaPlayer>)>,
+    mut get_player: Query<&mut Transform, With<LocaPlayer>>,
+    get_cam: Query<&Transform, (With<Camera3d>, Without<LocaPlayer>)>,
 ) {
     println!("noob");
 } 
